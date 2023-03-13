2c284a6bac995427f63e0beda59eca153ab432f6
ArtemOsuskyi <a.osuskiy@gmail.com>
2023-03-13 23:43:01 +0200

    updated prisma entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module }        from '@nestjs/common';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { EventsModule } from './events/events.module';
 import { AppService }    from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaService, PrismaModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule }   from './app.module';
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   
   await app.listen(3000);
 }
