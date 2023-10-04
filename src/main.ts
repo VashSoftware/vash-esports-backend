75ead679a3262c68d80d7d6cae8f5f3992005ecf
Stan Runge <stan@stanrunge.dev>
2023-10-04 23:11:22 +0200

    Update nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma/prisma.service';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -10,19 +8,8 @@ async function bootstrap() {
     origin: '*',
   });
 
-  const options = new DocumentBuilder()
-    .setTitle('Vash API')
-    .setDescription('Vash API description')
-    .addTag('vash')
-    .addServer(`http://localhost:3000/api`)
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api/swagger', app, document);
-
   app.setGlobalPrefix('api/');
-  const prismaService = app.get(PrismaService);
-  await app.enableShutdownHooks();
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT || 3000);
 }
 
